541e822c9ef8195669e11abe4fad4dda1a2e4b58
sakshi090-ops <prakashsakshi090@gmail.com>
2023-09-26 21:28:57 +0530

    All projects.

---
+++ practice-tic-tac-toe/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+
+//font-awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+//Toaster
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { NavigationComponent } from './components/navigate_bar/navigation/navigation.component';
+
+@NgModule({
+  declarations: [AppComponent, IconComponent, NavigationComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
